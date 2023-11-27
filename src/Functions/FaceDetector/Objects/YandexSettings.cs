133e5027f7e140e01d9b8296f2605aba419d042a
TimSon777 <dusalimov.tima@yandex.ru>
2023-11-27 18:45:40 +0300

    Remove unused fiels

--- src/Functions/FaceDetector/Objects/YandexSettings.cs
+++ src/Functions/FaceDetector/Objects/YandexSettings.cs
@@ -14,11 +14,7 @@ public sealed class YandexSettings
 
     public string FolderId { get; set; } = default!;
 
-<<<<<<< HEAD
-    public string SecretKey { get; set; } = default!;
-
     public string ApiKey { get; set; } = default!;
-=======
+
     public string Region { get; set; } = default!;
->>>>>>> f459cfdf3d72fd60485f6d2558974f30e45aa6da
 }
